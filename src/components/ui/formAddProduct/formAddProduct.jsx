364a0ac49bfa2e39c84cddf9152c8a9c4800d4fc
FaridDiazE <223185@ids.upchiapas.edu.mx>
2024-04-10 20:18:44 -0600

    Refactor

--- src/components/ui/formAddProduct/formAddProduct.jsx
+++ src/components/ui/formAddProduct/formAddProduct.jsx
@@ -1,16 +1,32 @@
 import './formAddProduct.css';
 import React from 'react';
 import GlobalInput from '../input/Input';
+import { useState } from 'react';
 
 export default function FormAddProduct() {
+
+    const [body, setBody] = useState({name :'' , model:'', size:'', price:'', brand:'', stock:'', category:'', imageURL:''})
+
+    const inputChange = ({ target }) => {
+        const { name, value } = target
+        setBody({
+            ...body,
+            [name]: value
+        })
+    
+
+
+
+
+
     return (
         <div className='registerProduct-content'>
             <div className='right-container'>
-                <GlobalInput type={'file'} withLabel={true} label={'Ingresa la imagen de tu archivo'} placeholder={''} custoClassInput={'input-registro'} />   
-                <GlobalInput type={'text'} withLabel={true} label={'nombre del articulo:'} placeholder={'nombre'} custoClassInput={'input-registro'} /> 
-                <GlobalInput type={'number'} withLabel={true} label={'precio del articulo'} placeholder={'$'} custoClassInput={'input-registro'} />  
-                <GlobalInput type={'number'} withLabel={true} label={'talla'} placeholder={'talla'} custoClassInput={'input-registro'} />   
-                <GlobalInput type={'number'} withLabel={true} label={'stock'} placeholder={'cantidad'} custoClassInput={'input-registro'} />   
+                <GlobalInput name="" onChange={} type={'file'} withLabel={true} label={'Ingresa la imagen de tu archivo'} placeholder={''} custoClassInput={'input-registro'} />   
+                <GlobalInput name="" onChange={} type={'text'} withLabel={true} label={'nombre del articulo:'} placeholder={'nombre'} custoClassInput={'input-registro'} /> 
+                <GlobalInput name="" onChange={} type={'number'} withLabel={true} label={'precio del articulo'} placeholder={'$'} custoClassInput={'input-registro'} />  
+                <GlobalInput name="" onChange={} type={'number'} withLabel={true} label={'talla'} placeholder={'talla'} custoClassInput={'input-registro'} />   
+                <GlobalInput name="" onChange={} type={'number'} withLabel={true} label={'stock'} placeholder={'cantidad'} custoClassInput={'input-registro'} />   
             </div>
             <div className='left-container'>
                 <div className='container-categories'>
--- src/components/ui/input/Input.jsx
+++ src/components/ui/input/Input.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Input.css';
 
-export default function GlobalInput({label, withLabel=true, type, placeholder,custoClassInput}) {
+export default function GlobalInput({label, withLabel=true, type, placeholder,custoClassInput,name,onChange}) {
   return (
     <>
         {withLabel && <label htmlFor="input" className='form-label'>{label}</label>}
-        <input type={type} placeholder={placeholder} id="form-control" className={custoClassInput} />
+        <input name={name} onChange={onChange} type={type} placeholder={placeholder} id="form-control" className={custoClassInput} />
     </>
   )
 }
